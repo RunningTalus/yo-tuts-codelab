9b4f42699cf6ee1075336c07b7b371c8d231be85
Dave May <dave@davemay.org>
2015-10-16 12:36:43 -0600

    addToDo() function added.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,17 @@
  * # MainCtrl
  * Controller of the mytodoApp
  */
+
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3',
+      'Item 4'
     ];
+    $scope.addTodo = function() {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
   });
