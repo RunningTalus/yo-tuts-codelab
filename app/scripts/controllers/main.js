0f7a7a7ec9e988f4850378ddcaea2e862bd23011
Dave May <dave@davemay.org>
2015-10-16 13:23:32 -0600

    Adding and removing todo items.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,14 +10,15 @@
 
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4'
-    ];
+    $scope.todos = [ 'Item 1', 'Item 2', 'Item 3', 'Item 4' ];
+
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
       $scope.todo = '';
     };
+
+    $scope.removeTodo = function(index) {
+      $scope.todos.splice(index, 1);
+    };
+
   });
