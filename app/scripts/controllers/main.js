afd67c31851bdc7dbbdd4ef263744195e25948d6
Dave May <dave@davemay.org>
2015-10-17 11:22:34 -0600

    Karma was erroring out on grunt test command. Added the following npm modules: grunt-karma, karma-phantomjs-launcher, karma-jasmine.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,7 @@
 
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = [ 'Item 1', 'Item 2', 'Item 3', 'Item 4' ];
+    $scope.todos = [];
 
     $scope.addTodo = function() {
       $scope.todos.push($scope.todo);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
   });
+
 });
